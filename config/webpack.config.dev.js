e5bd03289898f34bf3e71922c416c1cb8ec5c8e7
xinconan <xinconan2@sina.com>
2018-08-02 21:28:11 +0800

    feat: login

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -159,7 +159,7 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.css$/,
+            test: /\.(css|scss)$/,
             use: [
               require.resolve('style-loader'),
               {
@@ -188,6 +188,8 @@ module.exports = {
                   ],
                 },
               },
+              // scss
+              require.resolve('sass-loader'),
             ],
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,10 +1,62 @@
 import React from 'react';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import Logo from '../../logo.svg';
+import './index.scss';
+
+const FormItem = Form.Item;
 
 class Login extends React.Component{
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
   render(){
+    const { getFieldDecorator } = this.props.form;
     return (
-      <div>Login Page</div>
+      <div className="login-wrapper">
+        <header>
+          <a>
+            <img src={Logo} alt="Logo"/>
+            <span className="title">Ant Design Demo</span>
+          </a>
+        </header>
+        <div className="login-form-wrapper">
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <FormItem>
+              {getFieldDecorator('userName', {
+                rules: [{ required: true, message: 'Please input your username!' }],
+              })(
+                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+              })(
+                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('remember', {
+                valuePropName: 'checked',
+                initialValue: true,
+              })(
+                <Checkbox>记住密码</Checkbox>
+              )}
+              <a className="login-form-forgot" href="">忘记密码</a>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                登录
+              </Button>
+              或者 <a href="">立即注册</a>
+            </FormItem>
+          </Form>
+        </div>
+      </div>
     )
   }
 }
-export default Login;
+export default Form.create()(Login);;
