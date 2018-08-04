d943df14f6c644c386c9e6f776315a0cd143e5b5
xinconan <xinconan2@sina.com>
2018-08-04 21:35:10 +0800

    add todo

--- server/crawler/dbLottery.js
+++ server/crawler/dbLottery.js
@@ -1,12 +1,18 @@
 const mysql = require('../mysql');
 
+// 插入houseInfo信息
 const insertHouse = (houseInfo) => {
-  const {id, house_name, lottery_time, status} = houseInfo;
+  const {id, house_name, lottery_time, status, homeless_number, total_people, house_number, homeless_people} = houseInfo;
+  // TODO: 如果有该数据，直接更新
   return mysql('house').insert({
     id,
     house_name,
     lottery_time,
-    status
+    status,
+    homeless_number,
+    total_people,
+    house_number,
+    homeless_people
   })
 }
 
